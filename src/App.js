48986058410a04d7c9c91e7abebe798a791bf890
agkayster <ejike.chiboka@gmail.com>
2021-08-17 13:27:05 +0100

    list of hobbies

--- src/App.js
+++ src/App.js
@@ -3,91 +3,111 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<form>
-				<figure>
+			<form className='form'>
+				<figure className='zuriFig'>
 					<img
 						src='	https://internship.zuri.team/static/media/logo.36d2d48a.svg'
 						alt='brand logo'
 						class='brand-logo'></img>
 				</figure>
-				<header>
-					<h1>Ejike Chiboka</h1>
-					<p>No. 8, Lekki Phase 1, Lagos</p>
-					<p>Phone: 0903456222</p>
-					<p>email: abd@gmail.com</p>
+				<header className='resumeHeader'>
+					<h1 className='name'>Ejike Chiboka</h1>
+					<p className='address'>No. 8, Lekki Phase 1, Lagos</p>
+					<p className='phone'>Phone: 0803456222</p>
+					<p className='email'>email: ejike.chiboka@gmail.com</p>
 				</header>
-				<section>
-					<h3>
+				<section className='container'>
+					<h3 className='career'>
 						<u>CAREER OBJECTIVE</u>
 					</h3>
 					<hr />
-					<p>
-						I am seeking a company where my experience cannot only
-						improve the status of the company but also automate the
-						working process to the best possible one
+					<p className='teamPara'>
+						I am seeking a team where my experience cannot only
+						improve the status of the team but also learn from each
+						member of the team as best as possible
 					</p>
 					<hr />
-					<h3>
+					<h3 className='personalInfo'>
 						<u>PERSONAL INFORMATION</u>
 					</h3>
 					<hr />
-					<p>Date of Birth: April 18, 1890</p>
-					<p>Age: 22</p>
-					<p>Nickname: Dimex</p>
-					<p>Place of Birth: Ajegunle</p>
-					<p>Position Desired: M.D</p>
-					<p>Civil Status: Single</p>
-					<p>Religion: Christian</p>
+					<p className='birth'>Date of Birth: April 18, 1890</p>
+					<p className='age'>Age: 22</p>
+					<p className='nickname'>Nickname: Dimex</p>
+					<p className='place'>Place of Birth: Ajegunle</p>
+					<p className='desired'>Position Desired: M.D</p>
+					<p className='status'>Civil Status: Single</p>
+					<p className='religion'>Religion: Christian</p>
 					<hr />
-					<h3>
+					<h3 className='education'>
 						<u>EDUCATIONAL BACKGROUND</u>
 					</h3>
 					<hr />
-					<h4>
+					<h4 className='tertiary'>
 						<u>Tertiary Education</u>
 					</h4>
-					<p>
+					<p className='university'>
 						<b>School:</b> University of Owerri, Imo State
 					</p>
-					<p>
+					<p className='degree'>
 						<b>Degree:</b> B-Tech, Computer Science
 					</p>
-					<p>
+					<p className='graduation'>
 						<b>Year of Graduation:</b> 2014
 					</p>
-					<h4>
+					<h4 className='secondary'>
 						<u>Secondary Education</u>
 					</h4>
-					<p>
+					<p className='bariga'>
 						<b>School:</b> Bariga High School
 					</p>
-					<p>
+					<p className='green'>
 						<b>House:</b> Green House
 					</p>
 					<p>
-						<b>Year of Graduation:</b> 2007
+						<b className='yearOfGrad'>Year of Graduation:</b> 2007
 					</p>
 					<hr />
-					<h3>
+					<h3 className='work'>
 						<u>WORK EXPERIENCE</u>
 					</h3>
 					<hr />
-					<div>
-						<p>
+					<div className='allExperience'>
+						<p className='nstp'>
 							<b>NSTP Practise Teaching</b>
 						</p>
-						<p>Daycare Center Ogbomosho</p>
+						<p className='daycare'>Daycare Center Ogbomosho</p>
 						<p>Ilorin, Lagos</p>
 					</div>
-					<div>
-						<p>
+					<div className='moreExp'>
+						<p className='practise'>
 							<b>Practise Training</b>
 						</p>
-						<p>Caytero Agbero High School</p>
+						<p className='caytero'>Caytero Agbero High School</p>
 						<p>Obalende, Lagos</p>
+					</div>
+					<h3 className='hobbies'>
+						<u>HOBBIES</u>
+					</h3>
+					<hr />
+					<div className='allHobbies'>
+						<ul className="listHobby">
+							<li>Football </li>
+							<li>Swimming </li>
+							<li>Reading Books </li>
+							<li>Travelling</li>
+						</ul>
 					</div>
 				</section>
 			</form>
+			<footer className='zuriFooter'>
+				<a
+					href='https://internship.zuri.team/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					https://internship.zuri.team/
+				</a>
+			</footer>
 		</div>
 	);
 }
