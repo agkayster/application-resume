85ca6614571994d0663437ba1b13c203ab22f53a
agkayster <ejike.chiboka@gmail.com>
2021-08-17 12:19:55 +0100

    adding bold comments

--- src/App.js
+++ src/App.js
@@ -3,23 +3,76 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<header>
-				<h1>Ejike Chiboka</h1>
-				<p>No. 8, Lekki Phase 1, Lagos</p>
-				<p>Phone: 0903456222</p>
-				<p>email: abd@gmail.com</p>
-			</header>
-			<section>
-				<h3>
-					<u>Career Objective</u>
-				</h3>
-				<hr />
-				<p>
-					I am seeking a company where my experience cannot only
-					improve the status of the company but also automate the
-					working process to the best possible one
-				</p>
-			</section>
+			<form>
+				<header>
+					<h1>Ejike Chiboka</h1>
+					<p>No. 8, Lekki Phase 1, Lagos</p>
+					<p>Phone: 0903456222</p>
+					<p>email: abd@gmail.com</p>
+				</header>
+				<section>
+					<h3>
+						<u>CAREER OBJECTIVE</u>
+					</h3>
+					<hr />
+					<p>
+						I am seeking a company where my experience cannot only
+						improve the status of the company but also automate the
+						working process to the best possible one
+					</p>
+					<h3>
+						<u>PERSONAL INFORMATION</u>
+					</h3>
+					<hr />
+					<p>Date of Birth: April 18, 1890</p>
+					<p>Age: 22</p>
+					<p>Nickname: Dimex</p>
+					<p>Place of Birth: Ajegunle</p>
+					<p>Position Desired: M.D</p>
+					<p>Civil Status: Single</p>
+					<p>Religion: Christian</p>
+					<h3>
+						<u>EDUCATIONAL BACKGROUND</u>
+					</h3>
+					<hr />
+					<h4>
+						<u>Tertiary Education</u>
+					</h4>
+					<p>School: University of Owerri, Imo State</p>
+					<p>Degree: B-Tech, Computer Science</p>
+					<p>Year of Graduation: 2014</p>
+					<h4>
+						<u>Secondary Education</u>
+					</h4>
+					<p>
+						<b>School:</b> Bariga High School
+					</p>
+					<p>
+						<b>House:</b> Green House
+					</p>
+					<p>
+						<b>Year of Graduation:</b> 2007
+					</p>
+					<h3>
+						<u>WORK EXPERIENCE</u>
+					</h3>
+					<hr />
+					<div>
+						<p>
+							<b>NSTP Practise Teaching</b>
+						</p>
+						<p>Daycare Center Ogbomosho</p>
+						<p>Ilorin, Lagos</p>
+					</div>
+					<div>
+						<p>
+							<b>Practise Training</b>
+						</p>
+						<p>Caytero Agbero High School</p>
+						<p>Obalende, Lagos</p>
+					</div>
+				</section>
+			</form>
 		</div>
 	);
 }
