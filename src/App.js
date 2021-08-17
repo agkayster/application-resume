9782aa249e208cce2c14b1c943e49153de257ae8
agkayster <ejike.chiboka@gmail.com>
2021-08-17 12:02:35 +0100

    backing up to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header>
+				<h1>Ejike Chiboka</h1>
+				<p>No. 8, Lekki Phase 1, Lagos</p>
+				<p>Phone: 0903456222</p>
+				<p>email: abd@gmail.com</p>
+			</header>
+			<section>
+				<h3>
+					<u>Career Objective</u>
+				</h3>
+				<hr />
+				<p>
+					I am seeking a company where my experience cannot only
+					improve the status of the company but also automate the
+					working process to the best possible one
+				</p>
+			</section>
+		</div>
+	);
 }
 
 export default App;
